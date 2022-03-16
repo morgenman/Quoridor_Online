1847f5df3c91cc3eadf6a2c5d1deeb0d171e365c
Leland Sumner <lelandsumner@gmail.com>
2022-03-15 20:58:48 -0400

    added GET/POST simple test implementation

--- src/client/quoridor_site/quoridor_site_backend/views.py
+++ src/client/quoridor_site/quoridor_site_backend/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    html = "Click here to <a href=getName> Enter a Name </a>"
+    return HttpResponse(html)
+
+
+def getName(request):
+    return render(request, 'get_name.html')
+    
+
+def showName(request):
+    name = request.POST['name']
+    return render(request, 'show_name.html', {'name': name})    
