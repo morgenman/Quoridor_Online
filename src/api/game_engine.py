d38b256ed586910bcab8a9050dc9c4686f28446f
morgendc203 <morgendc203@potsdam.edu>
2022-03-15 20:09:46 -0400

    Decoding from board state implemented.

--- src/api/game_engine.py
+++ src/api/game_engine.py
@@ -16,18 +16,19 @@ def full_game_to_array(shorthand):
   print(moves) 
 
 def state_to_array(shorthand):
-  test()
   gameOut = game(9,2)
   gameOut.get(5,1).val = 1
   gameOut.get(5,9).val = 2
+  walls = [0,0,0,0]
   print(shorthand)
   temp = shorthand.split('/')
+  temp[0]=temp[0].strip()
+  temp[1]=temp[1].strip()
+  temp[2]=temp[2].strip()
+  temp[3]=temp[3].strip()
+  temp[4]=temp[4].strip()
 
   # Horizontal Walls
-  temp[0]=temp[0].strip()
-  for i in range(5):
-    print(temp[i])
-  print("["+temp[0]+"]")
   hori = [temp[0][i:i+2] for i in range(0, len(temp[0]), 2)]
   for i in hori:
     x = ord(i[0])-ord('`')
@@ -35,16 +36,24 @@ def state_to_array(shorthand):
     gameOut.get(x,y).set_wall_h()
 
   # Vertical Walls
-  temp[1]=temp[1].strip()
-  for i in range(5):
-    print(temp[i])
-  print("["+temp[1]+"]")
-  hori = [temp[1][i:i+2] for i in range(0, len(temp[1]), 2)]
-  for i in hori:
+  verti = [temp[1][i:i+2] for i in range(0, len(temp[1]), 2)]
+  for i in verti:
     x = ord(i[0])-ord('`')
     y = int(i[1])
     gameOut.get(x,y).set_wall_v()
 
+  # players
+  players = temp[2].split(' ')
+  for i in range(len(players)):
+    x = ord(players[i][0])-ord('`')
+    y = int(players[i][1])
+    gameOut.get(x,y).val = i+1
+
+  walls = temp[3].split(' ')
+  for i in range(len(walls)):
+    print("Player " +(i+1).__str__()+ " has " + walls[i].__str__() + " walls remaining.")
+
+  print("It is Player "+temp[4]+"'s turn.")
 
   gameOut.draw()
     
@@ -75,17 +84,19 @@ class tile:
     if self.w_west: return None
     return self.parent.get(self.x-1,self.y)
   def get_val(self):
-    if self.val == 0: return ' '
     if self.val == 1: return 'x'
     if self.val == 2: return 'o'
+    if self.val == 3: return '@'
+    if self.val == 4: return '*'
+    return ' '
   def set_wall_h(self):
-    print(self)
+    print("Horizontal wall at "+self.__repr__())
     self.get_north().w_south = True
-    self.w_north = True
+    self.w_north = True 
     self.get_east().get_north().w_south = True
     self.get_east().w_north = True
   def set_wall_v(self):
-    print(self)
+    print("Vertical wall at "+self.__repr__())
     self.get_north().get_east().w_west = True
     self.get_north().w_east = True
     self.get_east().w_west = True
