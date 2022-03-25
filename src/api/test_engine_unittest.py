106bf8090e22318833691dcf17d643263da05779
Allie Duguid <duguida203@potsdam.edu>
2022-03-24 22:24:04 -0400

    new tests for current move implementation, includes test for known issue. Also, basic python testing file, doesn't test much as of now

---
+++ src/api/test_engine_unittest.py
@@ -0,0 +1,15 @@
+import unittest
+
+from game_engine import game
+
+
+class TestGameEngine(unittest.TestCase):
+    def not_enough_players(self):
+        game1 = game(9, 1)  # not enough players
+
+    def too_many_players(self):
+        game2 = game(9, 5)  # too many players
+
+
+if __name__ == "__main__":
+    unittest.main()
