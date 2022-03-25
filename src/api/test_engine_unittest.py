53329c9dcf7bde72eb43fa9bd1e9174e2c5490e6
morgendc203 <morgendc203@potsdam.edu>
2022-03-25 09:39:20 -0400

    unit tests

--- src/api/test_engine_unittest.py
+++ src/api/test_engine_unittest.py
@@ -2,13 +2,17 @@ import unittest
 
 from game_engine import *
 
-
+# note, method names must start with 'test'
 class TestGameEngine(unittest.TestCase):
-    def not_enough_players(self):
-        game1 = game(9, 1)  # not enough players
-        
+    def test_num_players(self):
+        self.assertEqual(game(9, 4).get_num_players(), 4)
+        self.assertEqual(game(9, 3).get_num_players(), 3)
+        self.assertEqual(game(9, 2).get_num_players(), 2)
+        self.assertRaises(
+            AssertionError, game, 9, 1
+        )  # one is an invalid number of players
 
-    def too_many_players(self):
+    def test_too_many_players(self):
         game2 = game(9, 5)  # too many players
 
 
