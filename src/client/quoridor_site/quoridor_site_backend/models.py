0f56a02c6c0cb17cb3c5c2f30766ff56a4265457
Allie Duguid <duguida203@potsdam.edu>
2022-03-31 21:12:22 -0400

    Very simple games model

--- src/client/quoridor_site/quoridor_site_backend/models.py
+++ src/client/quoridor_site/quoridor_site_backend/models.py
@@ -1,17 +1,28 @@
 from django.db import models
+import uuid
 from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    wins = models.PositiveIntegerField(User, blank=True, null=True)
-    losses = models.PositiveIntegerField(User, blank=True, null=True)
+    wins = models.PositiveIntegerField(User, default=0)
+    losses = models.PositiveIntegerField(User, default=0)
 
     def __str__(self):
         return self.user.username
 
     def __int__(self):
-        return self.wins + " " + self.losses
+        return self.wins
+
+    def __int__(self):
+        return self.losses
 
     def save(self):
         super().save()
+
+
+class Games(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+
+    def __str__(self):
+        return self.id
