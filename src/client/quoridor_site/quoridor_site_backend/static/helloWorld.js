cd72953935695c3706701fd70ad686b6145c93d7
morgendc203 <morgendc203@potsdam.edu>
2022-04-14 13:40:27 -0400

    hwall

--- src/client/quoridor_site/quoridor_site_backend/static/helloWorld.js
+++ src/client/quoridor_site/quoridor_site_backend/static/helloWorld.js
@@ -21,7 +21,7 @@ function preload() {
   this.load.image('bg', '/static/Board.png');
   this.load.spritesheet('p1_idle', '/static/P1_Idle.png', { frameWidth: 64, frameHeight: 64, endFrame: 1 });
   this.load.spritesheet('p2_idle', '/static/P2_Idle.png', { frameWidth: 64, frameHeight: 64, endFrame: 1 });
-
+  this.load.spritesheet('h_wall', '/static/h_wall.png', { frameWidth: 192, frameHeight: 64, endFrame: 0 });
 }
 
 
@@ -56,6 +56,9 @@ function create() {
 
   let p1 = this.add.sprite(center.x + (0 * tile_size), center.y + ((4) * tile_size) - 15, 'p1_idle').play('p1_idle_animation');
   let p2 = this.add.sprite(center.x + (0 * tile_size), center.y + ((-4) * tile_size) - 15, 'p2_idle').play('p2_idle_animation');
+
+  let h_wall_1 = this.add.sprite(center.x + (1 * tile_size), center.y + ((2) * tile_size) - 40, 'h_wall');
+  
   p1.setScale(2);
   p2.setScale(2);
 
@@ -63,9 +66,10 @@ function create() {
 
 
 
+
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      this.add.rectangle(center.x + (i - 4) * tile_size, center.y + (j - 4) * tile_size, tile_size - 2, tile_size - 2, 0x000000 + 0x100000 * i + 0x001000 * j, 0.5);
+      //this.add.rectangle(center.x + (i - 4) * tile_size, center.y + (j - 4) * tile_size, tile_size - 2, tile_size - 2, 0x000000 + 0x100000 * i + 0x001000 * j, 0.5);
     }
   }
 
