ba9f02d098fbf5b73e609fcd0cd44031936ca73d
Leland Sumner <sumnerlm201@potsdam.edu>
2022-04-28 00:37:16 -0400

    removed numpy stuff and created table for storing games in database

--- src/api/http_server.py
+++ src/api/http_server.py
@@ -1,11 +1,54 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 import json
+import mysql.connector
 from game_engine import *
+import os
+
 
 hostName = "0.0.0.0"
 hostPort = 8080
 games = active_games()
+
+db = mysql.connector.connect(
+    user = 'root',
+    password = 'Bxu<!yGaY"tj={J7b3U[T],qm9Ms>^ZL',
+    host = os.getenv('DB_HOST'),
+    #user = os.getenv('DB_USER'),
+    #password = os.getenv('DB_PASSWORD'),
+    port = os.getenv('DB_PORT'),
+    #DB_HOST=db
+    #DB_USER=api
+    #DB_PASSWORD=password
+    #DB_NAME=quoridor_db
+    #DB_PORT=3306
+)
+#creates database if not already exists
+#db.cursor().execute("GRANT ALL PRIVILEGES ON *.* TO 'api'@'%' IDENTIFIED BY 'password'")
+db.cursor().execute('CREATE DATABASE IF NOT EXISTS active_games')
+
+db = mysql.connector.connect(
+    user = 'root',
+    password = 'Bxu<!yGaY"tj={J7b3U[T],qm9Ms>^ZL',
+    host = os.getenv('DB_HOST'),
+    #user = os.getenv('DB_USER'),
+    #password = os.getenv('DB_PASSWORD'),
+    database = 'active_games',
+    port = os.getenv('DB_PORT'),
+    #DB_HOST=db
+    #DB_USER=api
+    #DB_PASSWORD=password
+    #DB_NAME=quoridor_db
+    #DB_PORT=3306
+)
+
+db_cursor = db.cursor()
+
+db_cursor.execute("GRANT ALL PRIVILEGES ON *.* TO 'api'@'%'")
+db_cursor.execute("CREATE TABLE games (str_rep VARCHAR(128))")
+
+for x in db_cursor:
+    print(x)
 
 # MyServer hosts the game engine
 # Manual routing is in the match functions (match is switch statement for Python)
